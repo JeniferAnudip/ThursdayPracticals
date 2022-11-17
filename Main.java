a91254f1dfcfce87738bee1afbb8d586d4d89b12
Nida Fatima <65632560+nidafatima01@users.noreply.github.com>
2022-11-17 16:20:21 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+   public static void main(String[] args)
+   {
+     System.out.println("The Minimum value of Integer is :" +Integer.MIN_VALUE);
+     System.out.println("The Maximum value of Integer is :" +Integer.MAX_VALUE);
+     System.out.println("The Size of Integer is :" +Integer.SIZE);
+      }
+}
